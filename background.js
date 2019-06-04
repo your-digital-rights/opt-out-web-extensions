8d53ea8b2ca04702a1cd10bad43ee2e737c050de
Jonathan Holt <jonathan.a.holt@gmail.com>
2019-06-04 21:43:23 +0100

    make it compatible with firefox and chrome

--- background.js
+++ background.js
@@ -1,69 +1,87 @@
-// Copyright (c) 2018 Opt-out.eu. All rights reserved.
+// Copyright (c) 2019 Opt-out.eu. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var domainList = new Set();
-var selectedId = -1;
+let domainList = [];
 
-function loadDomains(){
-  domainList = new Set();
-  $.getJSON('https://api.opt-out.eu/companies', function(data) {
-    $.each( data, function(key, company) {
-      domainList.add(company.url);
-    });
+const whatBrowser = typeof browser !== "undefined" ? 'firefox' : 'chrome';
+const ext = (action) => {
+  if (typeof browser !== "undefined") {
+    return browser;
+  }
+
+  return chrome;
+}
+
+const loadDomains = () => {
+  fetch('https://api.opt-out.eu/companies')
+  .then((response) => {
+    return response.json();
+  })
+  .then((companies) => {
+    domainList = companies.map((company) => company.url);
   });
 };
 
-var xhr = new XMLHttpRequest();
-xhr.onload = function() {
-    var json = xhr.responseText;
-    json = JSON.parse(json);
+const openOptOutURL = (url) => {
+  const hostname = new URL(url).hostname;
+  const parsed = psl.parse(hostname);
+  const newURL = `https://opt-out.eu/?company=${parsed.domain}&pk_campaign=${whatBrowser}-extension`;
+
+  chrome.tabs.create({url: newURL});
 };
 
-function openOptOutURL(url){
-  var hostname = new URL(url).hostname;
-  var parsed = psl.parse(hostname);
-  var newURL = "https://opt-out.eu/?company=" + parsed.domain + "&pk_campaign=chrome-extension";
-  chrome.tabs.create({ url: newURL });
+const setTab = (tab, tabId) => {
+  const hostname = new URL(tab.url).hostname;
+  const parsed = psl.parse(hostname);
+
+  if (domainList.includes(parsed.domain)) {
+    ext().browserAction.enable(tabId);
+    ext().browserAction.setTitle({title: `Click to opt-out of ${parsed.domain}`, tabId});
+  } else {
+    ext().browserAction.disable(tabId);
+    ext().browserAction.setTitle({title: "This website is not currently supported", tabId});
+  }
 };
 
-function updateState(){
-  var tab = chrome.tabs.get(selectedId, function(tab){
-    var hostname = new URL(tab.url).hostname;
-    var parsed = psl.parse(hostname);
-    if (domainList.has(parsed.domain)) {
-      chrome.browserAction.enable(selectedId);
-      chrome.browserAction.setTitle({title: "Click to opt-out of " + parsed.domain, tabId: selectedId});
-    } else {
-      chrome.browserAction.disable(selectedId);
-      chrome.browserAction.setTitle({title: "This website is not currently supported", tabId: selectedId});
-    }
-})};
+const getTab = async (tabId) => {
+  if (typeof browser !== "undefined") {
+    const tab = await browser.tabs.get(tabId);
 
-chrome.runtime.onInstalled.addListener(function() {
+    setTab(tab, tabId);
+  } else {
+    chrome.tabs.get(tabId, (tab) => {
+      setTab(tab, tabId);
+    });
+  }
+};
+
+const updateState = (tabId) => {
+  if (tabId) {
+    getTab(tabId);
+  }
+};
+
+chrome.runtime.onInstalled.addListener(() => {
   loadDomains();
 });
 
-chrome.runtime.onStartup.addListener(function(){ 
+chrome.runtime.onStartup.addListener(() => {
   loadDomains();
 });
 
-chrome.tabs.onUpdated.addListener(function(tabId, props) {
-  if (props.status == "complete" && tabId == selectedId)
-    updateState();
+chrome.tabs.onUpdated.addListener((tabId, props) => {
+  updateState(tabId);
 });
 
-chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
-  selectedId = tabId;
-  updateState();
+chrome.tabs.onActivated.addListener((tab, props) => {
+  updateState(tab.tabId);
 });
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  selectedId = tabs[0].id;
-  updateState();
+chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+  updateState(tabs[0].id);
 });
 
-chrome.browserAction.onClicked.addListener(function(activeTab){ 
+chrome.browserAction.onClicked.addListener((activeTab) => {
   openOptOutURL(activeTab.url);
 });
-
